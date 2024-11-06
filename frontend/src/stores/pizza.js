75b8a6e77b6018efae4e490652b9584899e84a5a
xeniaTa <kseniatarsenko1@gmail.com>
2024-11-06 19:03:19 +0300

    module5-task3

--- frontend/src/stores/pizza.js
+++ frontend/src/stores/pizza.js
@@ -1,14 +1,30 @@
 import { defineStore } from 'pinia'
 export const usePizzaStore = defineStore('pizza', {
 	state: () => ({
-		pizza: {
-			name: "",
-			dough: {},
-			size: {},
-			sauce: {},
-			ingredients: [],
-		  },
+		name: "",
+		dough: {},
+		size: {},
+		sauce: {},
+		ingredients: [],
 	}),
-	getters: {},
+	getters: {
+		totalPizzaPrice: (state) => {
+			const ingredientsTotalPrice = state.ingredients.reduce((sum, ingredient) => sum + ingredient.price, 0);
+			const multiplier = state.size.multiplier;
+			return (multiplier * (state.sauce.price + state.dough.price + ingredientsTotalPrice));
+		},
+		getDough: (state)=> {
+			return state.dough;
+		},
+		getSize: (state)=> {
+			return state.size;
+		},
+		getSauce: (state)=> {
+			return state.sauce;
+		},
+		getIngredients: (state)=> {
+			return state.ingredients;
+		}
+	},
 	actions: {}
 })
--- frontend/src/stores/profile.js
+++ frontend/src/stores/profile.js
@@ -1,15 +1,33 @@
 import { defineStore } from 'pinia'
 export const useProfileStore = defineStore('profile', {
 	state: () => ({
-		profile: {
-			id: "",
-			name: "",
-			email: "",
-			avatar: "",
-			phone: "",
-			addresses:[]
-		},
+		id: 0,
+		name: "",
+		email: "",
+		avatar: "",
+		phone: "",
+		addresses:[],
+		orders: []
 	}),
-	getters: {},
+	getters: {
+		getAddresses: (state)=> {
+			return state.addresses;
+		},
+		getOrders: (state)=> {
+			return state.orders;
+		},
+		getName: (state)=> {
+			return state.name;
+		},
+		getEmail: (state)=> {
+			return state.email;
+		},
+		getAvatar: (state)=> {
+			return state.avatar;
+		},
+		getPhone: (state)=> {
+			return state.phone;
+		}
+	},
 	actions: {}
 })
