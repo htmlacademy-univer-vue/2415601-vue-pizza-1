36cdc19a614625b2cd6076a2da1a6971878c2e97
xeniaTa <kseniatarsenko1@gmail.com>
2024-11-06 23:20:18 +0300

    module6-task3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -18,9 +18,9 @@ export default defineConfig({
     host: true,
     port: 8080,
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        rewrite: (path) => path.replace(/^\/api/, ''),
+      "/api": {
+        target: "http://backend:3000/",
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
