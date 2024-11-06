a7ec68f0bfffbfb0d11e45cfbc981f478dd38594
xeniaTa <kseniatarsenko1@gmail.com>
2024-11-06 16:58:08 +0300

    m4t2

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,62 +1,15 @@
 <template>
-  <AppLayout>
-    <AppHeader></AppHeader>
-    <HomeView></HomeView>
-  </AppLayout>
+  <div class="main__wrapper">
+    <AppLayout>
+      <router-view />
+    </AppLayout>
+  </div>
 </template>
 
 <script setup>
 import AppLayout from "./layouts/AppLayout.vue";
-import AppHeader from "./layouts/AppHeader.vue"
-import HomeView from "./views/HomeView.vue";
 </script>
 
 <style lang="scss">
 @import "@/assets/scss/app.scss";
-body {
-  justify-content: center;
-  align-items: center;
-}
-.main__wrapper {
-  padding-bottom: 30px;
-
-  background-color: $white;
-  box-shadow: $shadow-light;
-
-  h1 {
-    margin-bottom: 0;
-    padding: 0 95px;
-
-    text-align: center;
-
-    @include b-s36-h42;
-  }
-
-  p {
-    padding: 0 95px;
-
-    text-align: center;
-
-    font-size: 20px;
-    line-height: 30px;
-  }
-
-  b {
-    font-size: 1.2em;
-  }
-}
-
-.main__header {
-  margin-bottom: 30px;
-  padding: 20px 0;
-
-  background-color: $green-600;
-
-  img {
-    display: block;
-
-    margin: 0 auto;
-  }
-}
-
 </style>
