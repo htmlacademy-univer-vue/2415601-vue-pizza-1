2db07d3e25750904954e8aeda42658261eefa56c
xeniaTa <kseniatarsenko1@gmail.com>
2024-11-06 19:29:18 +0300

    module5-task4

--- frontend/src/stores/pizza.js
+++ frontend/src/stores/pizza.js
@@ -1,30 +1,47 @@
-import { defineStore } from 'pinia'
-export const usePizzaStore = defineStore('pizza', {
-	state: () => ({
-		name: "",
-		dough: {},
-		size: {},
-		sauce: {},
-		ingredients: [],
-	}),
-	getters: {
-		totalPizzaPrice: (state) => {
-			const ingredientsTotalPrice = state.ingredients.reduce((sum, ingredient) => sum + ingredient.price, 0);
-			const multiplier = state.size.multiplier;
-			return (multiplier * (state.sauce.price + state.dough.price + ingredientsTotalPrice));
-		},
-		getDough: (state)=> {
-			return state.dough;
-		},
-		getSize: (state)=> {
-			return state.size;
-		},
-		getSauce: (state)=> {
-			return state.sauce;
-		},
-		getIngredients: (state)=> {
-			return state.ingredients;
-		}
-	},
-	actions: {}
-})
+import { defineStore } from "pinia";
+
+export const usePizzaStore = defineStore("pizza", {
+  state: () => ({
+    name: "",
+    dough: {},
+    size: {},
+    sauce: {},
+    ingredients: [],
+  }),
+  getters: {
+    totalPizzaPrice: (state) => {
+      const ingredientsTotalPrice = state.ingredients.reduce(
+        (sum, ingredient) => sum + ingredient.price,
+        0
+      );
+      const multiplier = state.size.multiplier;
+      return (
+        multiplier *
+        (state.sauce.price + state.dough.price + ingredientsTotalPrice)
+      );
+    },
+    getDough: (state) => {
+      return state.dough;
+    },
+    getSize: (state) => {
+      return state.size;
+    },
+    getSauce: (state) => {
+      return state.sauce;
+    },
+    getIngredients: (state) => {
+      return state.ingredients;
+    },
+  },
+  actions: {
+    addIngredient(newIngredient) {
+        const ingredientExist = state.ingredients.find((item) => item.id === newIngredient.id);
+    
+        if (ingredientExist) {
+            ingredientExist.quantity += 1;
+        } else {
+            state.ingredients.push({ ...newIngredient, quantity: 1 });
+        }
+    },
+}
+});
